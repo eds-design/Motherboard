020ad12bdacf0fd6210f7c198d5928dcc7ecc177
Georgi Georgiev <eds.designbg@gmail.com>
2026-09-18 18:16:25 +0300

    Add Bulgarian localization for core and bundled modules

---
+++ public_html/modules/cloudflare-turnstile/lang/bg-bg.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'module.cloudflare-turnstile.name' => 'Cloudflare Turnstile',
+    'module.cloudflare-turnstile.description' => 'Защитава формулярите за вход и възстановяване на парола с Cloudflare Turnstile.',
+    'module.cloudflare-turnstile.setup_go' => 'Отворете',
+    'module.cloudflare-turnstile.dashboard' => 'Таблото на Cloudflare',
+    'module.cloudflare-turnstile.setup_2' => 'Отворете Turnstile и създайте нов сайт',
+    'module.cloudflare-turnstile.setup_3' => 'Копирайте ключа за сайта (Site Key) и тайния ключ (Secret Key) в полетата по-горе',
+    'module.cloudflare-turnstile.setup_4' => 'Добавете домейна си към списъка с разрешени домейни',
+];
---
+++ public_html/modules/google-recaptcha/lang/bg-bg.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'module.google-recaptcha.name' => 'Google reCAPTCHA',
+    'module.google-recaptcha.description' => 'Защитава формулярите за вход и възстановяване на парола с Google reCAPTCHA v2.',
+    'module.google-recaptcha.setup_go' => 'Отворете',
+    'module.google-recaptcha.console' => 'Административната конзола на Google reCAPTCHA',
+    'module.google-recaptcha.setup_2' => 'Регистрирайте нов сайт с reCAPTCHA v2 и отметка „Не съм робот“',
+    'module.google-recaptcha.setup_3' => 'Добавете домейна си към списъка с разрешени домейни',
+    'module.google-recaptcha.setup_4' => 'Копирайте ключа за сайта (Site Key) и тайния ключ (Secret Key) в полетата по-горе',
+];
